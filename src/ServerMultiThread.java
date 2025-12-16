34211f325946ad7ab3c4699f3aeb678272380f72
gaia-mincigrucci <gaia.mincigrucci@gmail.com>
2025-12-16 09:46:37 +0100

    Update ServerMultiThread.java

--- src/ServerMultiThread.java
+++ src/ServerMultiThread.java
@@ -6,14 +6,16 @@ public class ServerMultiThread {
     public static void main(String[] args) throws IOException {
         int port = 12345;
         ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server avviato sulla porta " +port);
+        System.out.println("Server aaaavviato sulla porta " +port);
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
-            System.out.println("Nuovo client connesso: " + clientSocket.getInetAddress());
+            System.out.println("Nuovo client connesso: " + clientSocket.getInetAddress()
+            + ":"+ clientSocket.getPort());
 
             ClientHandler clientHandler = new ClientHandler(clientSocket);
             new Thread(clientHandler).start();
+
         }
     }
 }
